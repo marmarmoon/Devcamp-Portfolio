8ade00f78a04ca5b6ef6f7234f15eb429b69e407
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 14:43:28 +0000

    integreatd friendly routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -22,33 +22,33 @@ class PortfoliosController < ApplicationController
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
-  
-  def show
-  @portfolio_item = Portfolio.find(params[:id])
-end
 
-def destroy
-  # Perform the lookup
-  @portfolio_item = Portfolio.find(params[:id])
-  
-  # Destroy/delete the record
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Post was removed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
 
